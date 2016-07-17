8d1dfcfe93b592a1663ca93122d587c97a371f5d
Tony Beveridge <tony.beveridge@outlook.co.nz>
2016-07-18 10:54:36 +1200

    Split out some interfaces
    For use in Ng2 project

---
+++ Shared/Esoterica/IOWrapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace SharedObjects.Esoterica {
+
+    public interface IOWrapper {
+        Task<char> ReadCharacter();
+        Task<string> ReadString(string defaultIfEmpty = "");
+        Task Write(string src);
+        Task Write(char c);
+    }
+
+}
